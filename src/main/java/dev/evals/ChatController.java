c98cbd69d1365edb9cce6637889431911c6a6af6
Jettro Coenradie <jettro@coenradie.com>
2026-03-14 17:57:11 +0100

    Fixes langfuse integration and adds a controller

--- src/main/java/dev/evals/ChatController.java
+++ src/main/java/dev/evals/ChatController.java
@@ -2,9 +2,11 @@ package dev.evals;
 
 import dev.evals.model.ChatRequest;
 import dev.evals.model.ChatResponse;
+import dev.evals.model.WhiskySearchResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController( "/")
@@ -19,9 +21,17 @@ public class ChatController {
     }
 
     @PostMapping("/chat")
-    public ChatResponse chat(ChatRequest request) {
+    public ChatResponse chat(@RequestBody ChatRequest request) {
         logger.info("Received chat message: {}", request.message());
-        String chat = chatService.chat(request.message());
-        return new ChatResponse(chat);
+        var response = chatService.chat(request.message());
+        return new ChatResponse(response);
     }
+
+    @PostMapping("/chatrag")
+    public WhiskySearchResult chatRag(@RequestBody ChatRequest request) {
+        logger.info("Received chat rag message: {}", request.message());
+        var response = chatService.chatRAGTools(request.message());
+        return response.result();
+    }
+
 }
